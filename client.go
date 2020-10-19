1855b7c3805125d8b695d0065db2df71b93a918f
Andrey Malinovskiy <amalinovskiy@gmail.com>
2020-10-19 20:16:51 +0500

    client first version

---
+++ client.go
@@ -0,0 +1,74 @@
+package m3client
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var defaultAPIURL = "api/v1/query"
+
+type Client struct {
+	url string
+}
+
+func NewClient(host string) *Client {
+	return &Client{url: fmt.Sprintf("%s/%s", host, defaultAPIURL)}
+}
+
+func (c *Client) request(param map[string]string) []byte {
+
+	req, err := http.NewRequest("GET", c.url, nil)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	req.Header.Add("M3-Engine", "m3query")
+
+	q := req.URL.Query()
+
+	//http://localhost:7201/api/v1/query?query=count(http_requests)&time=1590147165
+
+	for p, v := range param {
+		q.Add(p, v)
+	}
+
+	req.URL.RawQuery = q.Encode()
+
+	httpClient := &http.Client{}
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatalf("err %v", err)
+	}
+
+	defer res.Body.Close()
+	responce, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return responce
+}
+
+func (c *Client) count(tag string, timepoint time.Time) []byte {
+	query := fmt.Sprintf("count(%s)", tag)
+
+	param := make(map[string]string)
+	param["query"] = query
+	param["time"] = string(timepoint.Unix())
+
+	return c.request(param)
+}
+func (c *Client) max(tag string, timepoint time.Time) []byte {
+	query := fmt.Sprintf("max(%s)", tag)
+
+	param := make(map[string]string)
+	param["query"] = query
+	param["time"] = string(timepoint.Unix())
+
+	return c.request(param)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module m3client
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.1
+	github.com/jhump/protoreflect v1.7.0
+	github.com/m3db/m3 v0.15.16
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0
+)
